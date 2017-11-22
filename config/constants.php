31ed17c189eb01e1ea74d944cb394c1bf45844a0
xingda <wxd598113636@gmail.com>
2017-11-22 20:35:53 +0000

    add globle variables on author information

---
+++ config/constants.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | User Defined Variables
+    |--------------------------------------------------------------------------
+    |
+    | This is a set of variables that are made specific to this application
+    | that are better placed here rather than in .env file.
+    | Use config('your_key') to get the values.
+    |
+    */
+
+    'author_name' => env('AUTHOR_NAME','Xingda'),
+    'author_email' => env('AUTHOR_EMAIL','wxd598113636@gmail.com'),
+
+
+];
