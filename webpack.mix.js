f199c1cd8e6fda3cfaab9572993ea6ffdd666cde
xingda <wxd598113636@gmail.com>
2017-11-22 20:37:52 +0000

    update bottom copyright scss file

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/base.scss', 'public/css')
+   .sass('resources/assets/sass/footer-copyright.scss', 'public/css')
    .sass('resources/assets/sass/app.scss', 'public/css');
