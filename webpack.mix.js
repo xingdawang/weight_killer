a59b797d213d75d5c0e5719635b6852d0a16f951
xingda <wxd598113636@gmail.com>
2017-11-23 00:32:28 +0000

    Introduce Bootswatch simple scss library

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,4 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/base.scss', 'public/css')
-   .sass('resources/assets/sass/footer-copyright.scss', 'public/css')
    .sass('resources/assets/sass/app.scss', 'public/css');
