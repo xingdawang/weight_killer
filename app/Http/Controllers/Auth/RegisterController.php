aefe2349510912b5ec3e6055f6d90c9b603bd1bf
xingda <wxd598113636@gmail.com>
2017-11-28 19:37:01 +0000

    change register return url to user/update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/user/update';
 
     /**
      * Create a new controller instance.
