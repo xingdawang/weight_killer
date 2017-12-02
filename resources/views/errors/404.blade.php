55fb85b1529049a876a3d03bae5081375f8c5e11
QingfengSun <franksun1996@gmail.com>
2017-12-02 11:43:21 +0000

    404notfound

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,8 +2,9 @@
 
 @section('content')
 <div class="jumbotron jumbotron-fluid">
-	<div class="container">
-		<p class="lead ">Whoops, looks like something went wrong</p>
+	<div class="container text-center">
+		<img class="center-block" src="{{ URL::asset('img/404.png') }}"/><br>
+		<p class="lead">Whoops, looks like something went wrong</p>
 	</div>
 </div>
 @endsection
