c97bf2504403ae5b38568ab0013ae64b832449bb
xingda <wxd598113636@gmail.com>
2017-12-12 19:01:08 +0000

    Redirect /sitemap.xml to /sitemap

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,11 @@ Route::get('user/weight', 'UserHealthController@add_today_weight')->name('user_v
 Route::post('user/store-weight', 'UserHealthController@store_today_weight')->name('user_record_weight');
 // Routing for timeline user weight, bmi and bfp.
 Route::get('/user/trendline', 'UserHealthController@get_weight_trendline')->name('user_weight_trendline');
+// Routing for sitemap.xml.
 Route::get('/sitemap', 'SitemapController@index');
+Route::get('/sitemap.xml', function(){
+	return redirect('sitemap');
+});
 // Routing for viewing contact_us 
 Route::get('/contact_us', function () {
     return view('contact_us');
